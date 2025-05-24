d6a6d26980bcb34129a7f7e907d34328d1fe90be
StifenKin <tomaspfarias@gmail.com>
2025-05-23 21:36:09 -0300

    Agregado de lo arbol

---
+++ src/main/java/lyc/compiler/tree/ASTManager.java
@@ -0,0 +1,43 @@
+package lyc.compiler.tree;
+
+public class ASTManager {
+
+/*    public static Integer expressionIdx;
+    public static Integer termIdx;
+    public static Integer factorIdx;
+    public static Integer programIdx;
+    public static Integer blockIdx;
+    public static Integer sentenceIdx;
+    public static Integer var_declarationIdx;
+    public static Integer var_sencente_decIdx;
+    public static Integer data_typeIdx;
+    public static Integer id_listIdx;
+    public static Integer decitionIdx;
+    public static Integer conditionIdx;
+    public static Integer comparisonIdx;
+    public static Integer comparatorIdx;
+    public static Integer iteratorId;
+    public static Integer assignmentIdx;
+    public static Integer s_writeIdx;
+    public static Integer s_readIdx;
+    public static Integer write_paramIdx;
+    public static Integer read_paramIdx;
+    public static Integer all_equalIdx;
+    public static Integer repeat_inlineIdx;
+    public static Integer comparable_listIdx;
+    public static Integer comparableIdx;
+    public static Integer obj_listIdx;
+
+ */
+
+    private static Integer root;
+
+    public static void setRoot(Integer r) {
+        root = r;
+    }
+
+    public static Integer getRoot() {
+        return root;
+    }
+}
+
